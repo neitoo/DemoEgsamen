ed82422977482014afa2ba79d3b2a6f14951140e
neitoo <samuraa855@gmail.com>
2022-12-21 14:39:51 +0500

    made by 50%

---
+++ Solution/RentalPointCPKiOv2/window/SellerWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using RentalPointCPKiOv2.Class;
+using RentalPointCPKiOv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RentalPointCPKiOv2.Database;
+
+namespace RentalPointCPKiOv2.window
+{
+    /// <summary>
+    /// Логика взаимодействия для SellerWindow.xaml
+    /// </summary>
+    public partial class SellerWindow : Window
+    {
+        public SellerWindow(Staff user)
+        {
+            InitializeComponent();
+            OrdersDataGrid.ItemsSource = DatabaseEntity.DBEntity.RenPointEnt.Orders.ToList();
+            DataContext = user;
+        }
+
+        private void quitAccBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClassWindow.createLoginWindow().Show();
+            this.Close();
+        }
+
+
+        private void listClientBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClassWindow.createListClientWindow().Show();
+        }
+    }
+}
